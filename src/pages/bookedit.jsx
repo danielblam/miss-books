03ffaf2d3763c221c9fd247ccf6dce7c275b4dce
Daniel Blam <danielblam29@gmail.com>
2026-03-08 13:15:16 +0200

    Fully implemented editing, and added failsafe default book list

--- src/pages/bookedit.jsx
+++ src/pages/bookedit.jsx
@@ -1,15 +1,19 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { bookService } from "../services/book.service";
 import { useNavigate, useParams } from "react-router-dom";
 
 export function BookEdit() {
 
     var { bookid } = useParams();
+    const navigate = useNavigate();
 
     var [data, setBookData] = useState(null)
+    var [inputs, setInputs] = useState({ currency: "ILS" })
+    var submitted = useRef(false)
+    var loadedInputs = useRef(false)
 
     useEffect(() => {
-        if(bookid != undefined) {
+        if (bookid != undefined) {
             fetchBook();
         }
     }, [])
@@ -19,28 +23,67 @@ export function BookEdit() {
         setBookData(book)
     }
 
-    var [inputs, setInputs] = useState({ currency: "ILS" })
-    const navigate = useNavigate();
-
     const handleChange = (e) => {
         const name = e.target.name;
         const value = e.target.value;
         setInputs(values => ({ ...values, [name]: value }))
     }
 
+    const loadInputs = () => {
+        console.log(data)
+        setInputs({
+            title: data.title,
+            author: data.authors.join(", "),
+            price: data.listPrice.amount,
+            currency: data.listPrice.currencyCode
+        })
+    }
+
     const submit = async () => {
-        if(bookid) return // TODO: implement saving edited book
-        console.log(inputs)
         var keys = Object.keys(inputs)
         var required = ["currency", "title", "author", "price"]
         for (const name of required) {
             if (!keys.includes(name) || inputs[name] == '') return
         }
-        await bookService.createBook(inputs)
-        navigate("/books", { state: { refresh: true } })
+        if (!bookid) {
+            await bookService.createBook(inputs)
+            navigate("/books", { state: { refresh: true } })
+        }
+        else {
+            console.log(data.title)
+            submitted.current = true
+            setBookData(values => ({
+                ...values,
+                title: inputs.title,
+                authors: inputs.author.split(", "),
+                listPrice: {
+                    amount: inputs.price,
+                    currencyCode: inputs.currency,
+                    isOnSale: data.listPrice.isOnSale
+                }
+            }))
+        }
     }
 
-    if(bookid != undefined && !data) return <div>Loading...</div>
+    useEffect(() => {
+        async function saveEditedBook() {
+            await bookService.save(data)
+            navigate("/books", { state: { refresh: true } })
+        }
+        if(submitted.current) saveEditedBook() 
+        if(!loadedInputs.current && data != null) { // load book data into inputs only once
+            loadedInputs.current = true
+            loadInputs()
+        }
+    }, [data])
+
+    if (bookid != undefined) {
+        if (!data) return <div>Loading...</div>
+    }
+
+    if(submitted.current) {
+        return <h1>Submitting...</h1>
+    }
 
     return (
         <div className="book-edit-container">
@@ -50,14 +93,14 @@ export function BookEdit() {
                 className="book-edit-form-input text-input"
                 name="title"
                 onChange={handleChange}
-                value={!data ? inputs.title : data.title}
+                value={inputs.title}
             />
             <div className="book-edit-form-title">Author:</div>
             <input
                 className="book-edit-form-input text-input"
                 name="author"
                 onChange={handleChange}
-                value={!data ? inputs.author : data.authors.join(", ")}
+                value={inputs.author}
             />
             <div className="book-edit-form-title">Price:</div>
             <div className="book-edit-input-group">
@@ -66,8 +109,11 @@ export function BookEdit() {
                     type="number"
                     name="price"
                     onChange={handleChange}
+                    value={inputs.price}
                 />
-                <select className="book-edit-form-input" name="currency" onChange={handleChange}>
+                <select
+                    className="book-edit-form-input" name="currency" onChange={handleChange}
+                    value={inputs.currency}>
                     <option>ILS</option>
                     <option>USD</option>
                     <option>EUR</option>
